9c0bc47423f6691c48e3504bc918aa4c08a368d1
NotKaiser <97164185+Kaiserrrrrr@users.noreply.github.com>
2026-03-05 15:27:00 +0800

    Update install.sh

--- dist/install.sh
+++ dist/install.sh
@@ -18,7 +18,7 @@ sudo pacman -S --noconfirm --needed $DRIVERS $LXQT $UTIL
 
 { [[ ! -f /etc/systemd/zram-generator.conf ]] && echo -e "[zram0]\nzram-size = ram * 0.6\ncompression-algorithm = zstd\nswap-priority = 100\nfs-type = swap" | sudo tee /etc/systemd/zram-generator.conf || true; }
 sudo systemctl daemon-reload && sudo usermod -aG video,audio,lp,scanner $USER
-{ [[ "$IS_LAPTOP" == "1" ]] && sudo systemctl enable tlp }
+{ [[ "$IS_LAPTOP" == "1" ]] && sudo systemctl enable tlp; }
 { [[ "$IS_LAPTOP" == "1" && "$CPU_VENDOR" =~ "GenuineIntel" ]] && sudo systemctl enable thermald; }
 sudo systemctl enable lightdm bluetooth NetworkManager
 sudo -u "$(logname)" systemctl --user enable --now pipewire pipewire-pulse wireplumber
