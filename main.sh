c48be4fc8ca28f58c06cdd61ee6038c35c9378d7
NotKaiser <97164185+Kaiserrrrrr@users.noreply.github.com>
2026-03-05 22:20:12 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
+reboot_now() { read -p "Reboot now? [Y/n]: " res; case "$res" in [Yy]*) sudo reboot ;; [Nn]*) exit 0 ;; *) exit 1 ;; esac } 
+
 URL="https://raw.githubusercontent.com/Kaiserrrrrr/aqtive/main/dist"
 
 if pgrep -x "lxqt-session" > /dev/null; then
     curl -fsSL "$URL/install.sh" | sh && \
     curl -fsSL "$URL/clean.sh" | sh && \
-    echo -e "\e[32m[✓] Aqtive Update Complete.\e[0m" && sync
+    sync && echo -n "[✓] Aqtive Update Complete. " && reboot_now
+    
 else
     curl -fsSL "$URL/install.sh" | sh && \
     curl -fsSL "$URL/config.sh" | sh && \
     curl -fsSL "$URL/clean.sh" | sh && \
-    echo -e "\e[33m[!] Aqtive Installation Complete. Rebooting in 5s to apply changes...\e[0m" && sync && sleep 3 && sudo reboot
+    sync && echo -n "[✓] Aqtive Installation Complete. " && reboot_now
 fi
