037c8a3d2b43562f8e527c08b1c4b79af7da0347
NotKaiser <97164185+Kaiserrrrrr@users.noreply.github.com>
2026-03-05 16:15:39 +0800

    Update install.sh

--- dist/install.sh
+++ dist/install.sh
@@ -2,7 +2,6 @@
 
 sudo pacman -Syu --noconfirm --needed
 
-USER0="${SUDO_USER: -$USER}"
 CPU_VENDOR=$(grep -m1 'vendor_id' /proc/cpuinfo)
 IS_LAPTOP=$([[ -d /sys/class/power_supply/BAT0 || -d /sys/class/input/mouse0 ]] && echo 1 || echo 0)
 DRIVERS="sof-firmware alsa-firmware mesa udisks2 zram-generator"
@@ -16,12 +15,5 @@ UTIL="breeze-icons gvfs xdg-utils xorg-server lightdm lightdm-gtk-greeter light-
 { grep -q "^\[multilib\]" /etc/pacman.conf && DRIVERS+=" lib32-mesa" || true; }
 
 sudo pacman -S --noconfirm --needed $DRIVERS $LXQT $UTIL
-
-{ [[ ! -f /etc/systemd/zram-generator.conf ]] && echo -e "[zram0]\nzram-size = ram * 0.6\ncompression-algorithm = zstd\nswap-priority = 100\nfs-type = swap" | sudo tee /etc/systemd/zram-generator.conf || true; }
-sudo systemctl daemon-reload && sudo usermod -aG video,audio,lp,scanner "$(logname)"
-{ [[ "$IS_LAPTOP" == "1" ]] && sudo systemctl enable tlp; }
-{ [[ "$IS_LAPTOP" == "1" && "$CPU_VENDOR" =~ "GenuineIntel" ]] && sudo systemctl enable thermald; }
-sudo systemctl enable lightdm bluetooth NetworkManager
-sudo -u "$USER0" systemctl --user enable --now pipewire pipewire-pulse wireplumber
 
 echo -e "\e[32m[✓] Installation Complete\e[0m"
