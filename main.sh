f5688a7ab31f776167305225786b29f2a544343f
NotKaiser <97164185+Kaiserrrrrr@users.noreply.github.com>
2026-03-06 11:57:17 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 URL="https://raw.githubusercontent.com/Kaiserrrrrr/aqtive/main/dist"
-reboot_now() { read -p "Reboot Now? [Y/n]: " res; case "$res" in [Yy]*) sudo reboot ;; [Nn]*) exit 0 ;; *) exit 1 ;; esac } 
+reboot_now() { read -p "Reboot Now? [Y/n]: " res; case "$res" in [Yy]*) sudo reboot ;; [Nn]*) exit 0 ;; *) exit 1 ;; esac; } 
 
 if pgrep -x "lxqt-session" > /dev/null; then
     curl -fsSL "$URL/install.sh" | sh && \
