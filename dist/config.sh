df1a7ca3c9acc58ad95444b350017884a19aa06c
NotKaiser <97164185+Kaiserrrrrr@users.noreply.github.com>
2026-03-06 21:56:43 +0800

    Update config.sh

--- dist/config.sh
+++ dist/config.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-echo -n "Configuring Machine..."
-
-{
+echo -n "Configuring Machine..." && {
 CPU_VENDOR=$(grep -m1 'vendor_id' /proc/cpuinfo)
 IS_LAPTOP=$(cat /sys/class/dmi/id/chassis_type | grep -qE '8|9|10|11|12|14|30|31|32' && echo 1 || echo 0)
 ACTIVE_SESSION=$(loginctl show-seat seat0 -p ActiveSession --value)
@@ -15,6 +13,4 @@ UID0=$(id -u "$USER0")
 { [[ "$IS_LAPTOP" == "1" ]] && sudo systemctl enable tlp; }
 { [[ "$IS_LAPTOP" == "1" && "$CPU_VENDOR" =~ "GenuineIntel" ]] && sudo systemctl enable thermald; }
 { sudo -u "$USER0" XDG_RUNTIME_DIR="/run/user/$UID0" systemctl --user enable --now pipewire pipewire-pulse wireplumber; } 
-} >/dev/null 2>&1
-
-echo -ne "\rConfiguration Complete\033[K"
+} >/dev/null 2>&1 && echo -ne "\rConfiguration Complete\033[K"
