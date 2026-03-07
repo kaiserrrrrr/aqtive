18fc1e0bc26e8ba9b151d32cfdf949ecb96d7f12
NotKaiser <97164185+Kaiserrrrrr@users.noreply.github.com>
2026-03-07 15:16:24 +0800

    Update install.sh

--- dist/install.sh
+++ dist/install.sh
@@ -5,7 +5,7 @@ CPU_VENDOR=$(grep -m1 'vendor_id' /proc/cpuinfo)
 IS_LAPTOP=$(cat /sys/class/dmi/id/chassis_type | grep -qE '8|9|10|11|12|14|30|31|32' && echo 1 || echo 0)
 DRIVERS="sof-firmware alsa-firmware mesa udisks2 zram-generator"
 LXQT="lxqt-session lxqt-panel lxqt-runner lxqt-qtplugin lxqt-globalkeys lxqt-notificationd lxqt-config lxqt-policykit lxqt-powermanagement lxqt-themes pcmanfm-qt qterminal lximage-qt screengrab qps openbox xdg-desktop-portal-lxqt"
-UTIL="gnu-free-fonts adobe-source-han-sans-otc-fonts ttf-joypixels breeze-icons gvfs xdg-utils xorg-server lightdm lightdm-gtk-greeter light-locker pipewire-audio pipewire-pulse alsa-utils bluez bluez-utils blueman networkmanager network-manager-applet"
+UTIL="gnu-free-fonts adobe-source-han-sans-otc-fonts noto-fonts-emoji breeze-icons gvfs xdg-utils xorg-server lightdm lightdm-gtk-greeter light-locker pipewire-audio pipewire-pulse alsa-utils bluez bluez-utils blueman networkmanager network-manager-applet"
 
 { [[ "$CPU_VENDOR" =~ "GenuineIntel" ]] && DRIVERS+=" intel-ucode vulkan-intel intel-media-driver thermald" || DRIVERS+=" amd-ucode"; }
 { lspci -n | grep -q "14e4:" && DRIVERS+=" broadcom-wl" || true; }
